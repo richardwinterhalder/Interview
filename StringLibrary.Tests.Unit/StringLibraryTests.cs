53c9a6b74823501a32105ca994ef26ef3589b447
richard.winterhalder <richard.winterhalder@rubicon.eu>
2023-01-27 11:09:05 +0100

    added Libraryclass and Test class

---
+++ StringLibrary.Tests.Unit/StringLibraryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringLibrary.Tests.Unit
+{
+    [TestFixture]
+    public class StringLibraryTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
---
+++ StringLibrary/StringLibraryClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLibrary
+{
+    public class StringLibraryClass
+    {
+        public static string Reverse(string text) 
+        {
+            return text;
+        }
+    }
+}
